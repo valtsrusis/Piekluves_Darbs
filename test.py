1f50b21bf7b42d085ed26dc602d9f724d28c0233
Valts Rūsis <valts.rusis@ventspils.edu.lv>
2025-03-06 11:14:07 +0200

    tests

---
+++ test.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+greeting = tk.Label(text="Hello, Tkinter")
+greeting.pack()
+poga = tk.Button(text="poga", width=25, height=25, bg='magenta', fg='red')
+poga.pack()
+
+tk.mainloop()
+
