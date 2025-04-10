5c044c531814c18ee2796a42ee2f9e70049b45cc
Valts Rūsis <valts.rusis@ventspils.edu.lv>
2025-04-10 11:09:57 +0300

    api update

--- test.py
+++ test.py
@@ -4,5 +4,33 @@ greeting.pack()
 poga = tk.Button(text="poga", width=25, height=25, bg='magenta', fg='red')
 poga.pack()
 
+import googlemaps
+from datetime import datetime
+
+gmaps = googlemaps.Client(key="AIzaSyB9WNy4lIYhQTZAZcKS5etg-HmXHZo6J18")
+
+# Geocoding an address
+geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+
+# Look up an address with reverse geocoding
+reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
+
+# Request directions via public transit
+now = datetime.now()
+directions_result = gmaps.directions("Sydney Town Hall",
+                                     "Parramatta, NSW",
+                                     mode="transit",
+                                     departure_time=now)
+
+# Validate an address with address validation
+addressvalidation_result =  gmaps.addressvalidation(['1600 Amphitheatre Pk'], 
+                                                    regionCode='US',
+                                                    locality='Mountain View', 
+                                                    enableUspsCass=True)
+
+# Get an Address Descriptor of a location in the reverse geocoding response
+address_descriptor_result = gmaps.reverse_geocode((40.714224, -73.961452), enable_address_descriptor=True)
+
+
 tk.mainloop()
 
--- test_datubaze.py
+++ test_datubaze.py
@@ -1,20 +1,11 @@
-import sqlite3
+import mysql.connector
 
-# Connect to an SQLite database (or create it if it doesn't exist)
-conn = sqlite3.connect('example.db')
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  password="yourpassword"
+)
 
-# Create a cursor object using the cursor() method
-cursor = conn.cursor()
+mycursor = mydb.cursor()
 
-# Create table
-cursor.execute('''CREATE TABLE IF NOT EXISTS stocks
-             (date text, trans text, symbol text, qty real, price real)''')
-
-# Insert a row of data
-cursor.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
-
-# Save (commit) the changes
-conn.commit()
-
-# Close the connection
-conn.close()
+mycursor.execute("CREATE DATABASE mydatabase")
