2108decf0bcf3ea301ff41add17a16dbcd9a39c9
valtsrusis <91083114+valtsrusis@users.noreply.github.com>
2025-04-13 22:00:08 +0300

    Update test_datubaze.py

--- test_datubaze.py
+++ test_datubaze.py
@@ -2,10 +2,18 @@ import mysql.connector
 
 mydb = mysql.connector.connect(
   host="localhost",
-  user="yourusername",
-  password="yourpassword"
+  user="myusername",
+  password="mypassword",
+  database="mydatabase"
 )
 
 mycursor = mydb.cursor()
 
-mycursor.execute("CREATE DATABASE mydatabase")
+sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+val = ("John", "Highway 21")
+
+mycursor.execute(sql, val)
+
+mydb.commit()
+
+print(mycursor.rowcount, "record inserted.")
