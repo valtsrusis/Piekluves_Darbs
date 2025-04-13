e982d6921f88686e70747c30e15561deb06b8bd0
valtsrusis <91083114+valtsrusis@users.noreply.github.com>
2025-04-13 23:59:54 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,36 +1,23 @@
 import tkinter as tk
+import sqlite3
 greeting = tk.Label(text="Hello, Tkinter")
 greeting.pack()
-poga = tk.Button(text="poga", width=25, height=25, bg='magenta', fg='red')
+
+def fetch_db():
+    connection = sqlite3.connect('products.db')
+    c = connection.cursor()
+    c.execute("SELECT * FROM products WHERE name='laptop'")
+    all_products = c.fetchall()
+    print(all_products[0])
+    connection.close()
+
+def load_frame2():
+    poga.tkraise()
+    fetch_db()
+
+poga = tk.Button(text="poga", width=25, height=25, bg='magenta', fg='red', command=lambda:load_frame2())
 poga.pack()
 
-import googlemaps
-from datetime import datetime
-
-gmaps = googlemaps.Client(key="API_KEY")
-
-# Geocoding an address
-geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
-
-# Look up an address with reverse geocoding
-reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
-
-# Request directions via public transit
-now = datetime.now()
-directions_result = gmaps.directions("Sydney Town Hall",
-                                     "Parramatta, NSW",
-                                     mode="transit",
-                                     departure_time=now)
-
-# Validate an address with address validation
-addressvalidation_result =  gmaps.addressvalidation(['1600 Amphitheatre Pk'], 
-                                                    regionCode='US',
-                                                    locality='Mountain View', 
-                                                    enableUspsCass=True)
-
-# Get an Address Descriptor of a location in the reverse geocoding response
-address_descriptor_result = gmaps.reverse_geocode((40.714224, -73.961452), enable_address_descriptor=True)
 
 
 tk.mainloop()
-
