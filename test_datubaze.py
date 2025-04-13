4cb9f9ac1a613e2cb514af6f2e9cbd3495fbdbba
valtsrusis <91083114+valtsrusis@users.noreply.github.com>
2025-04-14 00:00:19 +0300

    Update test_datubaze.py

--- test_datubaze.py
+++ test_datubaze.py
@@ -1,14 +1,19 @@
 import sqlite3
 
-connection = sqlite3.connect('students.db')
+connection = sqlite3.connect('products.db')
 
 c = connection.cursor()
 
-c.execute("""CREATE TABLE students  (
-            name TEXT,
-            age INTEGER,
-            height REAL
-    )""")
+#c.execute("""CREATE TABLE products  (
+#            name TEXT,
+#            price REAL
+#    )""")
 
-connection.commit()
-connection.close()
+#c.execute("INSERT INTO products VALUES ('mouse', 60)")
+
+#all_products = [('monitor  $', 90.99), ('laptop', 800.01), ('personal computer', 1000)]
+
+#c.executemany("INSERT INTO products VALUES (?, ?)", all_products)
+
+#connection.commit()
+#connection.close()
