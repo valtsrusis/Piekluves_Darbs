13aaa1a9fc11bb5dcc60992a156ece26777155af
valtsrusis <91083114+valtsrusis@users.noreply.github.com>
2025-04-15 00:37:56 +0300

    datubāzes aujauninājums

--- test_datubaze.py
+++ test_datubaze.py
@@ -1,19 +1,59 @@
 import sqlite3
 
-connection = sqlite3.connect('products.db')
 
-c = connection.cursor()
+#Query the DB and return all records
+def show_all():
+    connection = sqlite3.connect('login.db')
 
-#c.execute("""CREATE TABLE products  (
-#            name TEXT,
-#            price REAL
-#    )""")
+    c = connection.cursor()
 
-#c.execute("INSERT INTO products VALUES ('mouse', 60)")
+    c.execute("SELECT * FROM login_information")
+    items = c.fetchall()
 
-#all_products = [('monitor  $', 90.99), ('laptop', 800.01), ('personal computer', 1000)]
+    for item in items:
+        print(item)
+    
+    connection.commit()
 
-#c.executemany("INSERT INTO products VALUES (?, ?)", all_products)
+    connection.close()
 
+
+def add_one(id,first,last,email,password):
+    connection = sqlite3.connect('login.db')
+    c = connection.cursor()
+    c.execute("INSERT INTO login_information VALUES (?,?,?,?,?)", (id,first,last,email,password))
+    connection.commit()
+    connection.close()
+
+
+def delet_one(id):
+    connection = sqlite3.connect('login.db')
+    c = connection.cursor()
+    c.execute("DELETE from login_information WHERE id = (?)", id)
+    connection.commit()
+    connection.close()
+
+
+"""Lai sqlite_sequence neatkārto iepriekš izdzēstiem informāciju komanda formiem"""
+
+#connection = sqlite3.connect('login.db')
+#c = connection.cursor()
+#c.execute("DELETE from login_information WHERE id = (26)")
+#c.execute("UPDATE sqlite_sequence SET seq = 0 WHERE name = 'login_information'")
 #connection.commit()
 #connection.close()
+
+
+#c.execute("""CREATE TABLE IF NOT EXISTS login_information  (
+#            id integer PRIMARY KEY AUTOINCREMENT,
+#            username TEXT UNIQUE,
+#            surname TEXT,
+#            email TEXT,
+#            password TEXT
+#    )""")
+#
+#connection.commit()
+#connection.close()
+
+#c.execute("INSERT INTO login_information VALUES ('1', 'Jānis', 'Bulgerts', 'janis.bulgerts@gmail.com', 'lielais')")
+#c.execute("INSERT INTO login_information VALUES ('2', 'Jēkabs', 'Origs', 'jekabs.origs@gmail.com', 'lielie')")
